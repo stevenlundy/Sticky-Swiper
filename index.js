0a67964b398cea027273222446b37e21e7a3b122
Steven Lundy <steven.lundy@gmail.com>
2019-03-09 10:08:11 -0800

    add points that are tracked if you go near them

--- index.js
+++ index.js
@@ -8,36 +8,75 @@ class StickySwiper {
     let onCancel = config.onCancel || onUp;
 
     let mouseDown = false;
+    let path = [];
+    let touchedPoints = [];
+
+    let stickyPoints = config.stickyPoints || [];
+    let contactRadius = config.contactRadius || 10;
+
+    let getDistance = function(p1, p2) {
+      return ((p1.x - p2.x)**2 + (p1.y - p2.y)**2)**0.5;
+    };
+
+    let getPointsInContact = function(point, points, contactRadius) {
+      return points.filter(function(p) {
+        return getDistance(p, point) < contactRadius;
+      });
+    };
+
+    let getClosestPoint = function(point, points) {
+      return points.reduce(function(closest, current) {
+        return (getDistance(current, point) < getDistance(closest, point)) ? current : closest;
+      }, points[0]);
+    };
 
     let getMousePosition = function(e, context) {
       var x = (e.changedTouches ? e.changedTouches[0].pageX : e.pageX) - context.offsetLeft;
       var y = (e.changedTouches ? e.changedTouches[0].pageY : e.pageY) - context.offsetTop;
       e.preventDefault();
       return {x, y};
-    }
+    };
+
+    let handleNewPoint = function(point) {
+      path.push(point);
+      let contactPoints = getPointsInContact(point, stickyPoints, contactRadius);
+      let closestPoint = getClosestPoint(point, contactPoints);
+      let lastTouchedPoint = touchedPoints[touchedPoints.length-1];
+      if (closestPoint && closestPoint !== lastTouchedPoint) {
+        touchedPoints.push(closestPoint);
+      }
+    };
 
     let press = function(e) {
-      let {x, y} = getMousePosition(e, this);
+      let pos = getMousePosition(e, this);
+      handleNewPoint(pos);
       mouseDown = true;
-      onDown(el, x, y);
+      onDown(el, path, touchedPoints);
     };
 
     let drag = function(e) {
       if (!mouseDown) { return };
-      let {x, y} = getMousePosition(e, this);
-      onMove(el, x, y);
+      let pos = getMousePosition(e, this);
+      handleNewPoint(pos);
+      onMove(el, path, touchedPoints);
     };
 
     let release = function(e) {
-      let {x, y} = getMousePosition(e, this);
+      let pos = getMousePosition(e, this);
+      handleNewPoint(pos);
       mouseDown = false;
-      onUp(el, x, y);
+      onUp(el, path, touchedPoints);
+      path = [];
+      touchedPoints = [];
     };
 
     let cancel = function(e) {
-      let {x, y} = getMousePosition(e, this);
+      let pos = getMousePosition(e, this);
+      handleNewPoint(pos);
       mouseDown = false;
-      onCancel(el, x, y);
+      onCancel(el, path, touchedPoints);
+      path = [];
+      touchedPoints = [];
     };
 
     // Add mouse event listeners to element
@@ -54,9 +93,23 @@ class StickySwiper {
   }
 };
 
-function drawPoint(canvas, x, y) {
+let path = [];
+let otherPoints = [];
+
+function drawPoint(canvas, path, touchedPoints) {
+  let lastPoint = path[path.length-1];
   let context = canvas.getContext("2d");
-  context.strokeRect(x, y, 1, 1);
+  context.beginPath();
+  context.lineWidth = 2;
+  context.moveTo(path[0].x, path[0].y);
+  path.forEach(function(p) {
+    context.lineTo(p.x, p.y);
+  });
+  context.stroke();
+  touchedPoints.forEach(function(p) {
+    context.fillStyle = 'green';
+    context.fillRect(p.x-5, p.y-5, 10, 10);
+  });
 }
 
 function clearCanvas(canvas) {
@@ -67,7 +120,8 @@ function clearCanvas(canvas) {
 const config = {
   onDown: drawPoint,
   onUp: clearCanvas,
-  onMove: drawPoint
+  onMove: drawPoint,
+  stickyPoints: [{x: 10, y: 10}, {x: 90, y: 10}, {x: 90, y: 90}, {x: 10, y: 90}]
 }
 
 new StickySwiper(myCanvas, config);
